6154657cf5e7421909b80b741fb997191e81ce70
karthikk <getkarthikin@gmail.com>
2019-11-17 18:01:11 +1100

    WIP: initial commit

---
+++ src/main/java/com/thillguy/aoedata/AoedataApplication.java
@@ -0,0 +1,13 @@
+package com.thillguy.aoedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoedataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thillguy/aoedata/AoedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thillguy.aoedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoedataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
